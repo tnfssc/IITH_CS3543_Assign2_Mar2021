e35c0672cfead20b093b3c1d388c455745f4a203
tnfssc <tnfssc@gmail.com>
2021-03-08 11:16:35 +0530

    Parametrized filepath

--- client/index.go
+++ client/index.go
@@ -6,17 +6,18 @@ import (
 	"io"
 	"net"
 	"os"
-	"strings"
+	"path"
 )
 
 const BufferSize = 1024
 
-func sendFileToServer(fileName string, connection net.Conn) {
+func sendFileToServer(filePath string, connection net.Conn) {
 	var currentByte int64 = 0
 	fmt.Println("sending")
 	fileBuffer := make([]byte, BufferSize)
 	var err error
-	file, _ := os.Open(strings.TrimSpace(fileName))
+	dir, fileName := path.Split(path.Clean(filePath))
+	file, _ := os.Open(path.Join(dir, fileName))
 	_, _ = connection.Write([]byte(fileName))
 	for err == nil || err != io.EOF {
 		n := 0
@@ -30,11 +31,15 @@ func sendFileToServer(fileName string, connection net.Conn) {
 }
 
 func main() {
-	var Addr string
-	flag.StringVar(&Addr, "d", "127.0.0.1:12345", "Specify destination. Default is 127.0.0.1")
-
+	var Addr = ""
+	var filePath = ""
+	flag.StringVar(&Addr, "d", "", "Specify destination. eg. '127.0.0.1'")
+	flag.StringVar(&filePath, "f", "", "Specify file path. eg. '"+path.Join("send", "awsm.txt")+"'")
 	flag.Parse()
-
+	if (Addr == "") || filePath == "" {
+		fmt.Println("Use --help")
+		os.Exit(1)
+	}
 	conn, _ := net.Dial("udp", Addr)
-	sendFileToServer("send/awsm.txt", conn)
+	sendFileToServer(filePath, conn)
 }
--- server/index.go
+++ server/index.go
@@ -7,16 +7,18 @@ import (
 	"io"
 	"net"
 	"os"
+	"path"
 	"strconv"
+	"strings"
 )
 
 const BufferSize = 1024
 
-func getFileFromClient(connection net.Conn) {
+func getFileFromClient(connection net.Conn, fileName string) {
 	var currentByte int64 = 0
 	fileBuffer := make([]byte, BufferSize)
 	var err error
-	file, _ := os.Create("receive/" + "awsm.txt")
+	file, _ := os.Create(path.Join("receive", fileName))
 	for err == nil || err != io.EOF {
 		_, _ = connection.Read(fileBuffer)
 		cleanedFileBuffer := bytes.Trim(fileBuffer, "\x00")
@@ -39,17 +41,22 @@ func connectionHandler(connection net.Conn) {
 	}
 	fmt.Println("command received: " + string(buffer))
 	fmt.Println("getting a file")
-	getFileFromClient(connection)
+
+	cleanedBuffer := bytes.Trim(buffer, "\x00")
+	fileName := strings.TrimSpace(string(cleanedBuffer))
+	getFileFromClient(connection, fileName)
 }
 
 func main() {
-	var Addr string
-	var Port string
-	flag.StringVar(&Addr, "l", "0.0.0.0", "Specify IP address. Default is 0.0.0.0")
-	flag.StringVar(&Port, "p", "12345", "Specify port. Default is 12345")
-
+	var Addr = ""
+	var Port = ""
+	flag.StringVar(&Addr, "l", "", "Specify IP address. eg. '0.0.0.0'")
+	flag.StringVar(&Port, "p", "", "Specify port. eg. '12345'")
 	flag.Parse()
-
+	if (Addr == "") || Port == "" {
+		fmt.Println("Use --help")
+		os.Exit(1)
+	}
 	PortNumber, err := strconv.Atoi(Port)
 	if err != nil {
 		fmt.Println("Use a valid port: ", err)
@@ -60,6 +67,6 @@ func main() {
 		IP:   net.ParseIP(Addr),
 	}
 	ser, _ := net.ListenUDP("udp", &addr)
-	fmt.Println("Listening on ", Addr, Port)
+	fmt.Println("Listening on ", Addr, ":", Port)
 	connectionHandler(ser)
 }
